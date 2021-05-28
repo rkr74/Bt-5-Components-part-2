c2a575a43140ad4d52e7d050f5c9d32e92115f37
rkr74 <79821794+rkr74@users.noreply.github.com>
2021-05-28 10:46:45 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+/*Popover*/
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+/*Popover*/
